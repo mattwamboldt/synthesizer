5a92846e9f38145fa1e7c541a191e3bf1c819fe6
Matt Wamboldt <mattwamboldt@hotmail.com>
2015-04-14 10:50:31 -0300

    Initial commit, messing around with sdl to later mess around with audio

---
+++ src/game/debug.cpp
@@ -0,0 +1,28 @@
+#include "debug.h"
+#include <cstdio>
+#include <windows.h>
+
+namespace Debug
+{
+	//TODO: Make this a circular flushing buffer so we can write larger messages
+	char buffer[4096];
+
+	//Writes out the given message to the log file
+	void log(int category, const char* fmt, ...)
+	{
+		va_list argptr;
+		va_start(argptr,fmt);
+		vsnprintf(buffer, 4096, fmt, argptr);
+		SDL_LogDebug(category, buffer);
+	}
+
+	//TODO: Set up categories append to the string when using the console
+	//Writes out the given message to the debug console
+	void console(int category, const char* fmt, ...)
+	{
+		va_list argptr;
+		va_start(argptr,fmt);
+		vsnprintf(buffer, 4096, fmt, argptr);
+		OutputDebugStringA(buffer);
+	}
+}
