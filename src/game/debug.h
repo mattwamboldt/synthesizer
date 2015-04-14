b3423182e7fd808e8a87e64e93cd99e033ff6c75
Matt Wamboldt <mattwamboldt@hotmail.com>
2015-04-14 10:50:31 -0300

    Initial commit, messing around with sdl to later mess around with audio

---
+++ src/game/debug.h
@@ -0,0 +1,26 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <SDL/SDL.h>
+
+namespace Debug
+{
+	enum Category
+	{
+		APPLICATION = SDL_LOG_CATEGORY_APPLICATION, //application
+		ERROR = SDL_LOG_CATEGORY_ERROR, //error log
+		SYSTEM = SDL_LOG_CATEGORY_SYSTEM, //system log
+		AUDIO = SDL_LOG_CATEGORY_AUDIO, //audio log
+		VIDEO = SDL_LOG_CATEGORY_VIDEO, //video log
+		RENDER = SDL_LOG_CATEGORY_RENDER, //render log
+		INPUT = SDL_LOG_CATEGORY_INPUT // input log
+	};
+
+	//Writes out the given message to the log file
+	void log(int category, const char* fmt, ...);
+
+	//Writes out the given message to the debug console
+	void console(int category, const char* fmt, ...);
+}
+
+#endif
---
+++ src/game/main.cpp
@@ -0,0 +1,129 @@
+#include <SDL/SDL.h>
+#include <stdio.h>
+#include "debug.h"
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+    
+//The surface contained by the window
+SDL_Surface* gScreenSurface = NULL;
+
+//The image we will load and show on the screen
+SDL_Surface* gHelloWorld = NULL;
+
+//Starts up SDL and creates window
+bool init();
+bool loadMedia();
+void close();
+
+int main( int argc, char* args[] )
+{
+    //Start up SDL and create window
+    if( !init() )
+    {
+		Debug::console( SDL_LOG_CATEGORY_APPLICATION, "Failed to initialize!\n" );
+    }
+    else
+    {
+        //Load media
+        if( !loadMedia() )
+        {
+            Debug::console( SDL_LOG_CATEGORY_APPLICATION, "Failed to load media!\n" );
+        }
+        else
+        {
+			//Main loop flag
+            bool quit = false;
+
+            //Event handler
+            SDL_Event e;
+
+			//While application is running
+            while( !quit )
+            {
+				//Handle events on queue
+                while( SDL_PollEvent( &e ) != 0 )
+                {
+                    //User requests quit
+                    if( e.type == SDL_QUIT )
+                    {
+                        quit = true;
+                    }
+                }
+				
+				//Apply the image
+				SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+				//Update the surface
+				SDL_UpdateWindowSurface( gWindow );
+			}
+        }
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
+}
+
+bool init()
+{
+    //Initialization flag
+    bool success = true;
+
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_EVERYTHING ) < 0 )
+    {
+        Debug::console( SDL_LOG_CATEGORY_APPLICATION, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
+    }
+    else
+    {
+        //Create window
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
+        {
+            Debug::console( SDL_LOG_CATEGORY_APPLICATION, "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
+        }
+        else
+        {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
+        }
+    }
+
+    return success;
+}
+
+bool loadMedia()
+{
+    //Loading success flag
+    bool success = true;
+
+    //Load splash image
+    gHelloWorld = SDL_LoadBMP( "data/cat.bmp" );
+    if( gHelloWorld == NULL )
+    {
+        Debug::console( SDL_LOG_CATEGORY_APPLICATION, "Unable to load image %s! SDL Error: %s\n", "data/cat.bmp", SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //Deallocate surface
+    SDL_FreeSurface( gHelloWorld );
+    gHelloWorld = NULL;
+
+    //Destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
